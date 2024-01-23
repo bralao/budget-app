3ff7ce7bb68342ce070dd53a3ece67f3ebccca39
Bernardo Ralao <bernralao@gmail.com>
2024-01-23 14:12:39 +0000

    a

--- src/App.js
+++ src/App.js
@@ -11,47 +11,48 @@ import Remaining from './components/Remaining';
 
 const App = () => {
     return (
-        <AppProvider>
-          <div className='container'>
-            <h1 className='mt-3'>Shopping App</h1>
+      <AppProvider>
+        <div className='container'>
+          <h1 className='mt-3'>Shopping App</h1>
 
-            <div className='row mt-3'>
-              <div className='col-3'>
-                  <Budget />
-              </div>
-
-              <div className='col-3'>
-                <Remaining/>
-              </div>
-
-              <div className='col-3'>
-                <SpentSoFar/>
-              </div>
-
-              <div className='col-3'>
-                <Location />
-              </div>
+          <div className='row mt-3'>
+            <div className='col-3'>
+                <Budget />
             </div>
 
-
-            <h3 className='mt-3'>Shopping Cart</h3>
-
-            <div className='row '>
-              <div className='col-sm'>
-                <ExpenseList />
-              </div>
+            <div className='col-3'>
+              <Remaining/>
             </div>
 
-            <h3 className='mt-3'>Add Items</h3>
-
-            <div className='row mt-3'>
-              <div className='col-sm'>
-                <ItemSelected/>
-              </div>
+            <div className='col-3'>
+              <SpentSoFar/>
             </div>
 
+            <div className='col-3'>
+              <Location />
+            </div>
           </div>
-        </AppProvider>
+
+
+          <h3 className='mt-3'>Shopping Cart</h3>
+
+          <div className='row '>
+            <div className='col-sm'>
+              <ExpenseList />
+            </div>
+          </div>
+
+          <h3 className='mt-3'>Add Items</h3>
+
+          <div className='row mt-3'>
+            <div className='col-sm'>
+              <ItemSelected/>
+            </div>
+          </div>
+
+        </div>
+      </AppProvider>
     );
 };
+
 export default App;
